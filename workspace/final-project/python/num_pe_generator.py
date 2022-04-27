ff2744a09b20da34e4d0aadc9c93546f1fc3a265
Albert Garcia <agarcia0@mit.edu>
2022-04-27 18:31:48 -0400

    actually removed file now LOL

--- workspace/final-project/python/num_pe_generator.py
+++
@@ -1,7 +0,0 @@
-import random
-def num_PE_generator(meshX, min_PE, max_PE):
-  return random.randint(min_PE/meshX, max_PE/meshX) * meshX
-
-if __name__ == "__main__":
-  for _ in range(10):
-    print (num_PE_generator(14, 168/2, 168*2))
