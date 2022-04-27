693f6245ad4e4b7812deb385be82c2a6c6386388
Albert Garcia <agarcia0@mit.edu>
2022-04-27 18:29:08 -0400

    renamed file, added yaml_generator()

---
+++ workspace/final-project/python/iso_area.py
@@ -0,0 +1,25 @@
+import random
+import yaml
+
+def num_PE_generator(meshX, min_PE, max_PE):
+  return random.randint(min_PE/meshX, max_PE/meshX) * meshX
+
+def yaml_generator(filename, num_PEs, buffer_parameters):
+   with open(filename, 'r') as file:
+      data = yaml.safe_load(file)
+   
+   pe_buffer_size = data['architecture']['subtree'][0]['subtree'][0]['subtree'][0]['name']
+   pe_buffer_size = 'PE[0...' + str(num_PEs) + ']'
+
+   shared_glb = data['architecture']['subtree'][0]['subtree'][0]['local'][0]
+   shared_glb_attr = shared_glb['attributes']
+
+   for key, val in buffer_parameters.items():
+      shared_glb_attr[key] = val
+
+   with open('new.yaml', 'w', encoding='utf8') as file:
+      yaml.dump(data, file, default_flow_style=False, allow_unicode=True)
+
+if __name__ == "__main__":
+  for _ in range(10):
+    print (num_PE_generator(14, 168/2, 168*2))
